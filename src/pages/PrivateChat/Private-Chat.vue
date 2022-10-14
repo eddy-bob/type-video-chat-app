6173c86fdf4b8d3e27d1adb96b795d5c3b2fca6b
eddybob <eddymadu90@gmail.com>
2022-10-14 23:02:47 +0100

    fix:text alignment

--- src/pages/PrivateChat/Private-Chat.vue
+++ src/pages/PrivateChat/Private-Chat.vue
@@ -424,7 +424,7 @@ onBeforeUnmount(() => {
             ></p>
           </div>
           <div>
-            <p class="font-extrabold text-[16px] lg:mt-0 mt-4">
+            <p class="font-extrabold text-[16px] lg:mt-0 mt-3">
               {{
                 privateUserProfileData?.firstName
                   ? privateUserProfileData?.firstName +
